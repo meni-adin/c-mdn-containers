9853365a80f8953c2af607865be029935d981aeb
Menachem Adin <meniadin@gmail.com>
2025-10-25 16:14:41 +0300

    Add basic vector NewAndDelete test
    Replace hello test with simple lifecycle test that validates mdn_Vector_new
    and mdn_Vector_delete functions work correctly

--- test/vector_test/vector_test.cpp
+++ test/vector_test/vector_test.cpp
@@ -21,8 +21,16 @@ public:
     }
 };
 
-TEST_F(VectorTest, Hello) {
-    mdn_Vector_hello();
+TEST_F(VectorTest, NewAndDelete) {
+    mdn_Vector_t *vector = nullptr;
+    mdn_Status_t status;
+
+    status = mdn_Vector_new(&vector, sizeof(int), 10);
+
+    ASSERT_EQ(status, MDN_STATUS_SUCCESS);
+    ASSERT_NE(vector, nullptr);
+
+    mdn_Vector_delete(vector);
 }
 
 #ifdef MDN_CONTAINER_SAFE_MODE
